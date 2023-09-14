82eae3e0946d6e6924894972329ba70776f6ca08
Illia <illia.vasiliev@gmail.com>
2023-09-14 20:23:59 +0300

    UPD | deploy pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/dexola_camp_part2">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
