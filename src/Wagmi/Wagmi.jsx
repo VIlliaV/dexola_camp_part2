246d97615e6aca2df454a97673bff3c6cdde0d29
Illia <illia.vasiliev@gmail.com>
2023-09-23 05:24:23 +0300

    TRY | fix deploy

--- src/Wagmi/Wagmi.jsx
+++ src/Wagmi/Wagmi.jsx
@@ -1,11 +1,33 @@
-import { createWeb3Modal } from '@web3modal/wagmi/react';
+// import { createWeb3Modal } from '@web3modal/wagmi/react';
+// import { WagmiConfig } from 'wagmi';
+// import { wagmiConfig } from './wagmiConfig';
+
+// const { VITE_PROJECT_ID } = import.meta.env;
+// const projectId = VITE_PROJECT_ID;
+
+// createWeb3Modal({ wagmiConfig, projectId });
+
+// export const Wagmi = ({ children }) => {
+//   return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
+// };
+
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
 import { WagmiConfig } from 'wagmi';
-import { wagmiConfig } from './wagmiConfig';
+import { arbitrum, sepolia, mainnet } from 'wagmi/chains';
 
 const { VITE_PROJECT_ID } = import.meta.env;
 const projectId = VITE_PROJECT_ID;
+const chains = [mainnet, arbitrum, sepolia];
 
-createWeb3Modal({ wagmiConfig, projectId });
+const wagmiConfig = defaultWagmiConfig({
+  chains,
+  projectId,
+  appName: 'Web3Modal',
+  // publicClient,
+  // webSocketPublicClient,
+});
+
+createWeb3Modal({ wagmiConfig, projectId, chains });
 
 export const Wagmi = ({ children }) => {
   return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
--- src/Wagmi/wagmiConfig.js
+++ src/Wagmi/wagmiConfig.js
@@ -1,10 +1,10 @@
-import { defaultWagmiConfig } from '@web3modal/wagmi/react';
-import { sepolia, mainnet } from 'wagmi/chains';
-const { VITE_PROJECT_ID } = import.meta.env;
-const projectId = VITE_PROJECT_ID;
-const chains = [mainnet, sepolia];
-export const wagmiConfig = defaultWagmiConfig({
-  chains,
-  projectId,
-  appName: 'Web3Modal',
-});
+// import { defaultWagmiConfig } from '@web3modal/wagmi/react';
+// import { sepolia, mainnet } from 'wagmi/chains';
+// const { VITE_PROJECT_ID } = import.meta.env;
+// const projectId = VITE_PROJECT_ID;
+// const chains = [mainnet, sepolia];
+// export const wagmiConfig = defaultWagmiConfig({
+//   chains,
+//   projectId,
+//   appName: 'Web3Modal',
+// });
