1c1d0a73fc4683a16fa8f17fe77be34c0af63193
Illia <illia.vasiliev@gmail.com>
2023-10-27 13:51:26 +0300

    FIX | wagmi

--- src/Wagmi/Wagmi.jsx
+++ src/Wagmi/Wagmi.jsx
@@ -1,21 +1,44 @@
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
-import { WagmiConfig } from 'wagmi';
-import { sepolia, mainnet } from 'wagmi/chains';
+import { createWeb3Modal } from '@web3modal/wagmi/react';
+import { walletConnectProvider } from '@web3modal/wagmi';
+
+import { WagmiConfig, configureChains, createConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
 
 const { VITE_VERCEL_PROJECT_ID } = import.meta.env;
+
+const API_KEY = 'r4JOoXy2QHv7g5AWnAHdLeq8m8v1Y3y0';
 
 const projectId = VITE_VERCEL_PROJECT_ID;
 if (!projectId) {
   throw new Error('VITE_PROJECT_ID is not set');
 }
-const chains = [sepolia, mainnet];
-const wagmiConfig = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata: {
-    name: 'Web3Modal React',
-  },
+const { chains, publicClient } = configureChains(
+  [sepolia, mainnet],
+  [alchemyProvider({ apiKey: API_KEY }), walletConnectProvider({ projectId }), publicProvider()]
+);
+
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal react',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+};
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [
+    new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+    new InjectedConnector({ chains, options: { shimDisconnect: true } }),
+    new CoinbaseWalletConnector({ chains, options: { appName: metadata.name } }),
+  ],
+  publicClient,
 });
+
 createWeb3Modal({ wagmiConfig, projectId, chains });
 
 export const Wagmi = ({ children }) => {
